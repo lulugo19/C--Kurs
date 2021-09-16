d2a15b3c0d69cf8039ae5794d36f6c6061968f19
ununterhaltsam <kriegbenedek@gmail.com>
2021-09-16 18:56:45 +0200

    Satz des Pythagoras

---
+++ Satz-des-Pythagoras/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Satz_des_Pythagoras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+
+            |* 
+          a |  *   c
+            | .  *
+            -------*
+                b  
+            a² + b² = c²
+            c = Wurzel(a²+b²)
+            */
+
+            /*
+            EVA-Prinzip
+
+            1. Eingabe
+            2. Verarbeitung
+            3. Ausgabe
+            */
+
+            // Eingabe
+            Console.WriteLine("Satz des Pythagoras\n");
+            Console.Write("Gebe die Länge der ersten Seite a an: ");
+            double a = ZahlenEingabe();
+
+            Console.Write("Gebe die Länge der zweiten Seite b an: ");
+            double b = ZahlenEingabe();
+
+            // Verarbeitung
+            double c = Math.Sqrt( a*a + b*b );
+
+            // Ausgabe
+            Console.WriteLine($"Wurzel({a}²+{b}²) = {c}");
+        }
+
+        // liest ein Zahl von der Konsole ein
+        static double ZahlenEingabe()
+        {
+            string eingabe = Console.ReadLine();
+            double zahl = Convert.ToDouble(eingabe);
+            return zahl;
+
+        }
+
+
+    }
+}
